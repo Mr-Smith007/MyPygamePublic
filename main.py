ddd30f4a145337cb03d9c72188f62e939ddc8661
Mr-Smith007 <senior.smith007@gmail.com>
2025-01-04 14:13:56 +0000

    agregue el main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+
+pygame.init()
+pygame.display.set_mode((640, 480))
+pygame.display.set_caption("Ventana Basica")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT():
+            pygame.quit()
+            exit()
