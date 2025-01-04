ad1c3e1f4a28fd53dbcd992477934dd4f88d6984
Mr-Smith007 <senior.smith007@gmail.com>
2025-01-04 14:17:36 +0000

    modifique el main.py

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ pygame.display.set_caption("Ventana Basica")
 
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT():
+        if event.type == pygame.QUIT:
             pygame.quit()
             exit()
